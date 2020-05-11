52da2fa552e9e25136ea812c67b4ca5fc8fb0a9d
Valeria Melo <59574875+val-bit@users.noreply.github.com>
2020-05-10 21:23:02 -0300

    Add files via upload

---
+++ Media.c
@@ -0,0 +1,18 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+into dobro (int x);
+int main (int argc, char*argv[])
+{
+	int v;
+	system("CLS");
+	count <<"Digite o valor:";
+	cin>>v;
+	count<<"Dobro"= << dobro(v)<< endl;
+	system("PAUSE");
+	return EXIT_SUCCESS;
+}
+int dobro(int x)
+{
+	return(X*2);
+}
